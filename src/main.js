0986c19e1d4e89f4572e058ca2ae198b01043f34
shfwan <shafwan367@gmail.com>
2023-09-08 03:48:25 +0900

    vercel

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import dotenv from 'dotenv'
-import { app } from "./app/app.js";
+import express from 'express'
 import { logger } from './app/logging.js';
 
-dotenv.config()
 
 const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use('/home', (req,res) => res.send('Hello World'))
 
 app.listen(PORT, () => {
     logger.info(`Server listening on port ${PORT}`)
